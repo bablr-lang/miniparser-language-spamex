097a566e7f33900a518477770b4e1b6f1352a052
Conrad Buck <conartist6@gmail.com>
2023-09-05 19:11:22 -0600

    More tests

--- test/spamex.test.js
+++ test/spamex.test.js
@@ -135,8 +135,21 @@ describe('SPAM Expressions', () => {
         });
       });
 
-      it(`<| ID foo='bar'vroom='graarrr' |> throws`, () => {
-        expect(() => parse(`<| ID foo='bar'vroom='graarrr' |>`)).toThrowError();
+      it(`<| ID foo='bar'baz='quux' |> throws`, () => {
+        expect(() => parse(`<| ID foo='bar'baz='quux' |>`)).toThrowError();
+      });
+
+      it(`<| ID foo='bar' baz='quux' |>`, () => {
+        expect(parse(`<| ID foo='bar' baz='quux' |>`)).toEqual({
+          type: 'TokenTag',
+          tagType: 'ID',
+          value: null,
+          attrs: [
+            { type: 'Attribute', key: 'foo', value: 'bar' },
+            { type: 'Attribute', key: 'baz', value: 'quux' },
+          ],
+          args: [],
+        });
       });
 
       it(`<| ID 'moof'vroom='graarrr' |> throws`, () => {
@@ -211,31 +224,8 @@ describe('SPAM Expressions', () => {
         });
       });
 
-      it(`<| ID { re: // } |>`, () => {
-        expect(parse(`<| ID { re: // } |>`)).toEqual({
-          type: 'TokenTag',
-          tagType: 'ID',
-          value: null,
-          attrs: [],
-          args: [{ type: 'Argument', key: 're', value: emptyRegex }],
-        });
-      });
-
-      it(`<| ID { foo: 'bar', baz: 'quux' } |>`, () => {
-        expect(parse(`<| ID { foo: 'bar', baz: 'quux' } |>`)).toEqual({
-          type: 'TokenTag',
-          tagType: 'ID',
-          value: null,
-          attrs: [],
-          args: [
-            { type: 'Argument', key: 'foo', value: { type: 'String', value: 'bar' } },
-            { type: 'Argument', key: 'baz', value: { type: 'String', value: 'quux' } },
-          ],
-        });
-      });
-
-      it(`<| ID { kw: <| KW |> } |>`, () => {
-        expect(parse(`<| ID { kw: <| KW |> } |>`)).toEqual({
+      it(`<| ID { kw: <| KW |>, re: // } |>`, () => {
+        expect(parse(`<| ID { kw: <| KW |>, re: // } |>`)).toEqual({
           type: 'TokenTag',
           tagType: 'ID',
           value: null,
@@ -246,6 +236,103 @@ describe('SPAM Expressions', () => {
               key: 'kw',
               value: { type: 'TokenTag', tagType: 'KW', value: null, attrs: [], args: [] },
             },
+            { type: 'Argument', key: 're', value: emptyRegex },
+          ],
+        });
+      });
+    });
+
+    describe('NodeTag', () => {
+      it('< ID > throws', () => {
+        expect(() => parse('< ID >')).toThrowError();
+      });
+
+      it('<ID>', () => {
+        expect(parse('<ID>')).toEqual({
+          type: 'NodeTag',
+          tagType: 'ID',
+          attrs: [],
+          args: [],
+        });
+      });
+
+      it(`<ID foo='bar'>`, () => {
+        expect(parse(`<ID foo='bar'>`)).toEqual({
+          type: 'NodeTag',
+          tagType: 'ID',
+          attrs: [{ type: 'Attribute', key: 'foo', value: 'bar' }],
+          args: [],
+        });
+      });
+
+      it(`<ID foo='bar'baz='quux'> throws`, () => {
+        expect(() => parse(`<ID foo='bar'baz='quux'>`)).toThrowError();
+      });
+
+      it(`<ID foo='bar' baz='quux'>`, () => {
+        expect(parse(`<ID foo='bar' baz='quux'>`)).toEqual({
+          type: 'NodeTag',
+          tagType: 'ID',
+          attrs: [
+            { type: 'Attribute', key: 'foo', value: 'bar' },
+            { type: 'Attribute', key: 'baz', value: 'quux' },
+          ],
+          args: [],
+        });
+      });
+
+      it(`<ID {}>`, () => {
+        expect(parse(`<ID {}>`)).toEqual({
+          type: 'NodeTag',
+          tagType: 'ID',
+          attrs: [],
+          args: [],
+        });
+      });
+
+      it(`<ID {foo:'bar'}>`, () => {
+        expect(parse(`<ID {foo:'bar'}>`)).toEqual({
+          type: 'NodeTag',
+          tagType: 'ID',
+          attrs: [],
+          args: [{ type: 'Argument', key: 'foo', value: { type: 'String', value: 'bar' } }],
+        });
+      });
+
+      it(`<ID { foo: 'bar' }>`, () => {
+        expect(parse(`<ID { foo: 'bar' }>`)).toEqual({
+          type: 'NodeTag',
+          tagType: 'ID',
+          attrs: [],
+          args: [{ type: 'Argument', key: 'foo', value: { type: 'String', value: 'bar' } }],
+        });
+      });
+
+      it(`<ID foo='bar'{ foo: 'bar' }> throws`, () => {
+        expect(() => parse(`<ID foo='bar'{ foo: 'bar' }>`)).toThrowError();
+      });
+
+      it(`<ID foo='bar' { foo: 'bar' }>`, () => {
+        expect(parse(`<ID foo='bar' { foo: 'bar' }>`)).toEqual({
+          type: 'NodeTag',
+          tagType: 'ID',
+          attrs: [{ type: 'Attribute', key: 'foo', value: 'bar' }],
+          args: [{ type: 'Argument', key: 'foo', value: { type: 'String', value: 'bar' } }],
+        });
+      });
+
+      it(`<ID { kw: <KW>, re: // }>`, () => {
+        expect(parse(`<ID { kw: <KW>, re: // }>`)).toEqual({
+          type: 'NodeTag',
+          tagType: 'ID',
+          attrs: [],
+          args: [
+            {
+              type: 'Argument',
+              key: 'kw',
+              value: { type: 'NodeTag', tagType: 'KW', attrs: [], args: [] },
+            },
+            { type: 'Argument', key: 're', value: emptyRegex },
           ],
         });
       });
